88294be288262287e76b9397670125b17046b9e9
Santiago Zappa <santiagozappab@gmail.com>
2025-09-24 12:04:26 -0500

    dummy

--- .history/src/app/layout_20250924120336.tsx
+++ .history/src/app/layout_20250924120336.tsx
@@ -1,6 +1,4 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
